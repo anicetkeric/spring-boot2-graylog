e055df10886dd784d4812e91e17f727305f53eac
aek <anicetkeric@gmail.com>
2020-02-07 22:31:46 +0000

    initial commit

---
+++ src/main/java/com/logging/springboot2graylog/SpringBoot2GraylogApplication.java
@@ -0,0 +1,13 @@
+package com.logging.springboot2graylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2GraylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2GraylogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logging/springboot2graylog/SpringBoot2GraylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logging.springboot2graylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2GraylogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
